e147d4d090330e4c6c0c7e939af0bc0472d2e2d3
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-01-27 05:40:46 +0500

    sliders responsive now

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //  JavaScript Document
 $(document).ready(function () {
   $("#autoWidth").lightSlider({
-    autoWidth: true,
+    // autoWidth: true,
     loop: true,
     slideMargin: 40,
+    item: 4,
+    slideMargin: 10,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          item: 3,
+          slideMove: 1,
+          slideMargin: 10,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          item: 2,
+          slideMove: 1,
+          slideMargin: 10,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          item: 1,
+          slideMove: 1,
+        },
+      },
+    ],
     onSliderLoad: function () {
       $("#autoWidth").removeClass("cS-hidden");
     },
