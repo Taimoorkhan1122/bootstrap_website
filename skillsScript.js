79de9b7ed4358ca23cf7d88495e224a6c1e72f17
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-01-28 04:52:23 +0500

    Website completed On my end

--- skillsScript.js
+++ skillsScript.js
@@ -1,12 +1,37 @@
+// getting required DOM elements
 const tags = document.querySelectorAll("#tags span");
+const buttons = document.querySelectorAll("#technologies a");
+// array from nodelist object
+const btnArr = Array.from(buttons);
 
-// getting className of selected profile
-const fakeIDs = "cloud";
+// btn profile-button-primary
+// Getting all buttons inside technologies and adding event lister
+btnArr.map((item) => {
+  item.addEventListener("click", (event) => {
+    removeOldClasses();
+    event.target.classList.add("btn", "profile-button-primary");
+    addActiveClass(event);
+  });
+});
 
-// adding active class to selected ids
-console.log(tags);
-Array.from(tags).map((data) =>
-  data.classList.contains(fakeIDs)
-    ? data.classList.add("active")
-    : data.classList.remove("active")
-);
+// Add active class to select technology
+function addActiveClass(eventData) {
+  // getting className of selected profile
+  const ID = eventData.target.id;
+
+  // adding active class to selected ids
+  Array.from(tags).map((data) =>
+    data.classList.contains(ID)
+      ? data.classList.add("active")
+      : data.classList.remove("active")
+  );
+}
+
+//remove old classe
+function removeOldClasses() {
+  btnArr.map((item) => {
+    if (item.classList.contains("btn", "profile-button-primary")) {
+      item.classList.remove("btn", "profile-button-primary");
+    }
+  });
+}
